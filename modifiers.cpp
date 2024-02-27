b97427c269bd020f55500de1eacf66a72443bd3c
Mohamed Saud <86352274+mdsaud03@users.noreply.github.com>
2024-02-27 19:10:20 +0530

    DataType Modifiers

---
+++ modifiers.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	printf("size of integer = %ld\n",sizeof(int));
+	printf("size of character = %d\n",sizeof(char));
+	printf("size of float = %d\n",sizeof(float));
+	printf("size of double = %d\n",sizeof(double));
+	printf("size of short= %d\n",sizeof(short));
+	printf("size of long = %d\n",sizeof(long));
+	
+}
